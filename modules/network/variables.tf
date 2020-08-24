e5c889d8b79531c4c5b1d5772b164e874e2f9613
Peter Hrvola <peter.hrvola@hotmail.com>
2020-08-24 10:30:29 +0200

    Initial commit

---
+++ modules/network/variables.tf
@@ -0,0 +1,35 @@
+variable "project" {
+    type = string
+}
+
+variable "cluster_name" {
+    type = string
+
+}
+
+variable "location" {
+    type = string
+}
+
+variable "region" {
+    type = string
+}
+
+variable "project_services" {
+  type = list
+
+  default = [
+    "cloudresourcemanager.googleapis.com",
+    "servicenetworking.googleapis.com",
+    "container.googleapis.com",
+    "compute.googleapis.com",
+    "iam.googleapis.com",
+    "logging.googleapis.com",
+    "monitoring.googleapis.com",
+    "sqladmin.googleapis.com",
+    "securetoken.googleapis.com",
+  ]
+  description = <<-EOF
+  The GCP APIs that should be enabled in this project.
+  EOF
+}
---
+++ variables.tf
@@ -0,0 +1,37 @@
+variable "project" {
+  default = "identity-graph-285408"
+}
+
+variable "cluster_name" {
+  type    = string
+  default = "dev"
+}
+
+variable "location" {
+  type    = string
+  default = "europe-west1-c"
+}
+
+variable "region" {
+  type    = string
+  default = "europe-west1"
+}
+
+variable "project_services" {
+  type = list
+
+  default = [
+    "cloudresourcemanager.googleapis.com",
+    "servicenetworking.googleapis.com",
+    "container.googleapis.com",
+    "compute.googleapis.com",
+    "iam.googleapis.com",
+    "logging.googleapis.com",
+    "monitoring.googleapis.com",
+    "sqladmin.googleapis.com",
+    "securetoken.googleapis.com",
+  ]
+  description = <<-EOF
+  The GCP APIs that should be enabled in this project.
+  EOF
+}
